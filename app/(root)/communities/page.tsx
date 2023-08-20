14b4e534c0cd4d374cdcc0a331e95f3737b2ddfc
negi18kartik <negi18kartik@gmail.com>
2023-08-20 12:47:57 +0530

    Implemented Communities Features

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,9 +1,56 @@
-const Page = async () => {
+import {
+  fetchUser,
+  fetchUserPosts,
+  fetchUsers,
+} from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { profileTabs } from "@/constants";
+import Image from "next/image";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import UserCard from "@/components/cards/UserCard";
+import { fetchCommunities } from "@/lib/actions/community.actions";
+import CommunityCard from "@/components/cards/CommunityCard";
+
+async function Page() {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchCommunities({
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 20,
+  });
+
   return (
     <section>
-      <h1 className="head-text mb-8">Communities</h1>
+      <h1 className="head-text mb-8">Search</h1>
+      <div className="mt-12 flex flex-col gap-7">
+        {result.communities.length === 0 ? (
+          <p className="no-result">No Users</p>
+        ) : (
+          <>
+            {result.communities.map((community) => (
+              <CommunityCard
+                key={community.id}
+                id={community.id}
+                name={community.name}
+                username={community.username}
+                imgUrl={community.image}
+                bio={community.bio}
+                members={community.members}
+              />
+            ))}
+          </>
+        )}
+      </div>
     </section>
   );
-};
+}
 
 export default Page;
